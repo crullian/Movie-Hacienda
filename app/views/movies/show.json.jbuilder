d802cdc6b7e2d58465198be4c2fd0e31a828b479
crullian <crullian@gmail.com>
2014-03-23 09:13:42 -0400

    Initial commit

---
+++ app/views/movies/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @movie, :name, :description, :year, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MovieApp::Application.config.secret_key_base = '575abc210e319bac3c183e6448b90f0ff14bf2362c00f350283d3251c4013a6ceaecb73dbd53f0cb13b52ce8b01496d0ddc80a536c616d9699306eeeee9c1bb2'
